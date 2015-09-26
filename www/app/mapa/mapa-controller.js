f0d5457eef12b5a2b54cdcbf96914c5a3735cd2c
Domingos Junior <djunior_s@hotmail.com>
2015-09-26 00:03:16 -0300

    Adicionando infowindow

--- www/app/mapa/mapa-controller.js
+++ www/app/mapa/mapa-controller.js
@@ -22,13 +22,15 @@ angular.module('servnow-map')
 
     $scope.$on('mapInitialized', function(event, evtMap) {
       map = evtMap;
+      ctrl.map = map;
       angular.forEach(ctrl.markers, function(mar){
         var latLng = new google.maps.LatLng(mar.position[0], mar.position[1]);
-        var marker = new google.maps.Marker({position:latLng, customInfo: "Marker " + mar.photo});
+        var marker = new google.maps.Marker({position:latLng, icon:'img/coffee-marker.png', customInfo: "Marker " + mar.photo});
 
         google.maps.event.addListener(marker,"click", function(event) {
           console.log(event);
           console.log(marker);
+          map.scope.showInfoWindow.apply(this, [event, 'marker-info'])
         });
         ctrl.dynMarkers.push(marker);
       });
@@ -42,8 +44,7 @@ angular.module('servnow-map')
     function getCurrentPosition(){
       $ionicPlatform.ready(function(){
         $ionicLoading.show({
-          content: 'Carregando posição...',
-          showBackdrop: false
+          template: 'Carregando sua posição...'
         });
 
         var options = {
@@ -60,8 +61,7 @@ angular.module('servnow-map')
         }, function (err) {
           $ionicLoading.hide();
           $ionicLoading.show({
-            content: 'Error!! ' + err,
-            showBackdrop:false
+            template: 'Error!! ' + err
           });
 
           $timeout(function () {
@@ -83,6 +83,11 @@ angular.module('servnow-map')
           ctrl.markers = data;
           return ctrl.markers;
         });
+    }
+
+    function opa(event, cafe) {
+      ctrl.yeld = cafe;
+      ctrl.map.scope.showInfoWindow.apply(this, [event, 'marker-info'])
     }
   }
 })();
